2edad376cc690f92aebdae0de19e5e4f370fd45d
ethan.phan <ethan.phan@riotinto.com>
2022-12-07 08:56:04 +1000

    day 1 fix

--- day1.py
+++ day1.py
@@ -1,15 +1,14 @@
 with open("day1.txt", "r") as f:
-    lines = f.readlines()
+    lines = [l.strip() for l in f.readlines()]
 
-    deer_list = []
-    sum_deer = 0
-
-    for line in lines:
-        if line != '\n':
-            sum_deer = int(line.replace("\n","")) + sum_deer
-        else:
-            deer_list.append(sum_deer)
-            sum_deer = 0
+deer_list = []
+sum_deer = 0
+for line in lines:
+    if line != '':
+        sum_deer = int(line) + sum_deer
+    else:
+        deer_list.append(sum_deer)
+        sum_deer = 0
 
 deer_list.sort(reverse=True)
 print(deer_list[0])
